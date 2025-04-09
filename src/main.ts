f7b3fd1030b50bd42fb6f1283e5b4d058574dd2e
coding <cd62970@gmail.com>
2025-04-09 23:15:45 +0300

    Reinitialize repository and commit files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './comen/pipes/validation.pipe';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
