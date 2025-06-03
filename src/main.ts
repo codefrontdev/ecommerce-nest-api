918ed7711ca29a38c2dfcd32aaadd27336e62c9d
codefrontdev <cd62970@gmail.com>
2025-06-03 09:07:02 -0700

    llllllll

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
