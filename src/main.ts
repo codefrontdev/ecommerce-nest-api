a81426d3efbe1ad6414ca4f8f4f6938e6937fa39
codefrontdev <cd62970@gmail.com>
2025-06-14 15:14:19 -0700

    v2

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,26 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://192.168.1.4:5173',
+    'http://192.168.1.6:3000',
+    'https://ecommerce-dash-app.netlify.app',
+    'https://ecommerce-demo-v1.netlify.app',
+  ];
+  
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:3000',
-      'http://192.168.1.4:5173',
-      'http://192.168.1.6:3000',
-      'https://ecommerce-dash-app.netlify.app',
-      'https://ecommerce-demo-v1.netlify.app/',
-    ],
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
   });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
